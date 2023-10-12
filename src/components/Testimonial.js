fee405dcaa789f67af29f0936cd77bd7a75372a7
ShivamGupta15347 <shivamgupta15347@gmail.com>
2023-10-13 02:22:47 +0530

    mid

--- src/components/Testimonial.js
+++ src/components/Testimonial.js
@@ -1,11 +1,43 @@
 import React from "react";
 import Card from "./Card";
+import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
 
 const Testimonial = (props) => {
   let reviews = props.reviews;
+
+  function leftShiftHandler() {}
+
+  function rightShiftHandler() {}
+
+  function supriseHandler() {}
+
   return (
     <div>
-      <Card review={reviews[1]}></Card>
+      <Card review={reviews[0]}></Card>
+
+      <div className="flex text-3xl mt-5 gap-3 text-violet-400 font-bold mx-auto">
+        <button
+          onClick={leftShiftHandler}
+          className="cursor-pointer  text-violet-500"
+        >
+          <FiChevronLeft />
+        </button>
+        <button
+          onClick={rightShiftHandler}
+          className="cursor-pointer  text-violet-500"
+        >
+          <FiChevronRight />
+        </button>
+      </div>
+
+      <div>
+        <button
+          onClick={supriseHandler}
+          className="bg-violet-400 hover:bg-violet-500 transition-all duration-200 cursor-pointer px-10 py-2 rounded-md font-bold text-bold  text-white text-lg"
+        >
+          Surprise Me
+        </button>
+      </div>
     </div>
   );
 };
